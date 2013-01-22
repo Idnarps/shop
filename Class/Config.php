ced0a445d00a6de26a95e122be1dcc912b2f040f
alekseyskv <alekseyskv@gmail.com>
2013-01-22 22:00:01 +0400

    константы конфига

--- Class/Config.php
+++ Class/Config.php
@@ -6,8 +6,13 @@
  */
 class Class_Config{
 
- static public function DB() {
-   return new PDO('mysql:host=localhost;dbname=shop', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES "utf8"'));
- }
+  const DB_HOST = "localhost";
+  const DB_USER = "root";
+  const DB_PASS = "";
+  const DB_NAME = "shop";
+
+  static public function DB() {
+    return new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES "utf8"'));
+  }
 
 }
