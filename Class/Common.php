e121db02360058f1dd3dc0510631ece9d8010cd3
idnarps <idnarps@gmail.com>
2013-01-25 11:10:23 +0400

    Изменения в коммоне

--- Class/Common.php
+++ Class/Common.php
@@ -17,10 +17,14 @@ class Class_Common {
   protected $_db; // Подключение к БД
 
   protected $_sql; //SQL-запрос, созданный классом
+  
+  private $_stm;
 
-  public function __construct(){
+  public function __construct($tblName){
+    $this->_tblName = $tblName;
     $this->_db = Class_Config::DB();
     $this->_idColumn = 'id';
+    $this->_initDefaults();
   }
 
   /**
@@ -36,10 +40,10 @@ class Class_Common {
       return false;
     }
     try{
-      $this->_sql = 'INSERT INTO ' . $this->_tblName . ' SET ' . $this->_dataForInsert($data);
-      $stm = $this->_db->prepare($this->_sql);
-      $this->_bindArrayValue($stm, $data);
-      $stm->execute();
+      $this->_sql = 'INSERT INTO ' . $this->_tblName . ' AS ' . $this->_tblAlias . ' SET ' . $this->_dataForInsertUpdate($data);
+      $this->_stm = $this->_db->prepare($this->_sql);
+      $this->_bindArrayValue($this->_stm, $data);
+      $this->_stm->execute();
       $id = $this->_db->lastInsertId();
       if ($id) {
         return $id;
@@ -61,28 +65,63 @@ class Class_Common {
    *                                 'alias' => 'алиас для таблицы поля'(необязательное поле))
    * @param array|bool $leftJoin Массив со строками left join
    *                             Вид: array([] => 'ref_nomenclature AS rn ON rn.id=z.ref_nomenclature_id')
-   * @param bool $distinct Отображение флага DISTINCT в запросе
+   * @param array $params Массив с дополнительными параметрами запроса
+   *                      Вид: array([distinct] => 1|0,
+   *                                 [order] => 'название поля|полей с алисами таблицы',
+   *                                 [limit] => 'количество запрашиваемых записей')
    * @return array|bool Результат запроса к БД
    */
-  public function select($fields, $where, $leftJoin = false, $distinct = false) {
+  public function select($fields, $where, $leftJoin = false, $params = array()) {
     if (!is_array($fields)) {
       return false;
     }
-    $this->_initDefaults();
-    $this->_sql = 'SELECT ' . ($distinct ? 'DISTINCT' : '') . $this->_fromFields($fields) .
+    $this->_sql = 'SELECT ' . ($params['distinct'] ? 'DISTINCT' : '') . $this->_fromFields($fields) .
                   ' FROM ' . $this->_tblName . ' AS ' . $this->_tblAlias .
-                  $this->_addLeftJoin($leftJoin) . ' WHERE ' .$this->_dataForWhere($where);
+                  $this->_addLeftJoin($leftJoin) . ' WHERE ' .$this->_dataForWhere($where) .
+                  ($params['order'] ? ' ORDER BY ' . $params['order'] : '') .
+                  ($params['limit'] ? ' LIMIT ' . $params['limit'] : '');
     // Преобразуем входные данные для where для привязки значений к PDOStatment
     try{
-      $stm = $this->_db->prepare($this->_sql);
-      $this->_bindArrayValue($stm, $this->_prepareForBind($where));
-      $stm->execute();
-      return $stm->fetchAll(PDO::FETCH_ASSOC);
+      $this->_stm = $this->_db->prepare($this->_sql);
+      $this->_bindArrayValue($this->_stm, $this->_prepareForBind($where));
+      $this->_stm->execute();
+      return $this->_stm->fetchAll(PDO::FETCH_ASSOC);
     }
     catch (PDOException $e) {
       echo $e->getMessage();
       return false;
     }
+  }
+
+  /**
+   * @param $data Массив с данными для вставки в таблицу.
+   *              Вид: array('название поля в таблице' => 'значение',
+   *                         'название поля в таблице' => 'значение',
+   *                         ...)
+   * @param $where Ассоциативный массив или массив ассоциативных массивов с данными для выборки
+   *                      Вид: array('name' => значение,
+   *                                 'value' => '',
+   *                                 'sign' => 'знак сравнения'(необязательное поле),
+   *                                 'alias' => 'алиас для таблицы поля'(необязательное поле))
+   * @return mixed Количество обновлённых строк в БД|false в случае ошибки
+   */
+  public function update($data, $where) {
+    if (!is_array($data)) {
+      return false;
+    }
+    try{
+      $this->_sql = 'UPDATE ' . $this->_tblName . ' AS ' . $this->_tblAlias .
+                    ' SET ' . $this->_dataForInsertUpdate($data) .
+                    ' WHERE ' . $this->_dataForWhere($where);
+      $this->_stm = $this->_db->prepare($this->_sql);
+      $this->_bindArrayValue($this->_stm, array_merge($data, $this->_prepareForBind($where)));
+      $this->_stm->execute();
+    }
+    catch (PDOException $e) {
+      echo $e->getMessage();
+      return false;
+    }
+    return $this->getCountRows();
   }
 
   /**
@@ -107,6 +146,19 @@ class Class_Common {
     }
   }
 
+
+  /**
+   * Возвращает текущий PDOStatment объект
+   * @return object текущий PDOStatment объект
+   */
+  public function getStatement() {
+    return $this->_stm;
+  }
+
+  public function getCountRows() {
+    return $this->_stm->columnCount();
+  }
+
   /**
   * Метод для формирования тела вставки для директивы INSERT
   * @param $data Массив с данными для вставки в таблицу.
@@ -115,10 +167,10 @@ class Class_Common {
   *                         ...)
   * @return string Строку с параметрами для INSERT
   */
-  private function _dataForInsert($data) {
+  private function _dataForInsertUpdate($data) {
     $result = '';
     foreach ($data as $key => $val) {
-      $result[] = ' ' . $key . ' = :' . $key;
+      $result[] = ' ' . $this->_tblAlias . '.' . $key . ' = :' . $key;
     }
     return implode(',', $result);
   }
@@ -217,6 +269,17 @@ class Class_Common {
   }
 
   /**
+   * Метод для добавления отдельного выражения-условия отбора where
+   * @param $data массив с данными для условия
+   * @return string часть запроса where
+   */
+  private function _addWhereItem($data) {
+    $sign = $data['sign'] ? $data['sign'] : '=';
+    $name = ($data['alias'] ? $data['alias'] : $this->_tblAlias) . '.' . $data['name'];
+    return $name . ' ' . $sign . ' :' . $data['name'];
+  }
+
+  /**
    * Метод для перобразования м
    * @param $array массив который приходит для выражения where
    */
@@ -230,17 +293,6 @@ class Class_Common {
       $return[$array['name']] = $array['value'];
     }
     return $return;
-  }
-
-  /**
-   * Метод для добавления отдельного выражения-условия отбора where
-   * @param $data массив с данными для условия
-   * @return string часть запроса where
-   */
-  private function _addWhereItem($data) {
-    $sign = $data['sign'] ? $data['sign'] : '=';
-    $name = ($data['alias'] ? $data['alias'] : $this->_tblAlias) . '.' . $data['name'];
-    return $name . ' ' . $sign . ' :' . $data['name'];
   }
 
   /**
